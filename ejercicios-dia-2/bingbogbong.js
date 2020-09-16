3cc7c1a1a5ce367b104a5b180dc81da267ae3e34
lcarloszapatag <calitosf@yahoo.com>
2020-09-16 16:28:08 -0500

    ejercicio que muestra los multiplos de 3 y 5,mediante un juego

---
+++ ejercicios-dia-2/bingbogbong.js
@@ -0,0 +1,24 @@
+let number=prompt("ingrese un numero")
+if (number%3==0 &&number%5==0){
+
+  console.log("bingbong")
+}
+
+
+else if(number%3==0){
+
+console.log("bing")
+
+}
+else if(number%5==0){
+
+console.log("bong")
+
+}
+
+
+
+else{
+
+  console.log("No hay bing,bong y bingbong, el número es  "+ number + " ")
+}
