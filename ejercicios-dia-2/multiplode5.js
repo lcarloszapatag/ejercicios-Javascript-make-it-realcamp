81c9950cfb930e0ab8fad96e21e5d381d159b095
lcarloszapatag <calitosf@yahoo.com>
2020-09-16 13:20:57 -0500

    ejercicio multiplo de 5

---
+++ ejercicios-dia-2/multiplode5.js
@@ -0,0 +1,13 @@
+let number=prompt("escribir un numero y averigue si es multiplo 5")
+
+
+if(number%5==0){
+
+console.log("Si, el número  "+ number + " es múltiplo 5")
+
+}
+
+else{
+
+  console.log("No, el número "+ number + " no es múltiplo de 5")
+}
