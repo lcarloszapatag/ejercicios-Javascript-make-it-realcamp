47edfdcfc83ef79c020da85b71d15a74c2235e5b
lcarloszapatag <calitosf@yahoo.com>
2020-09-16 11:02:33 -0500

    Arreglar que el aleatorio funcione una vez y el juego tenga 6 oportunidades

--- ejercicios-dia-2/randomNumber.js
+++ ejercicios-dia-2/randomNumber.js
@@ -1,19 +1,30 @@
+let aleatorio =Math.floor(Math.random()*10+1) 
+// se mejoro el juego para que tuviera 6 oportunidades
+for(let i=1;i<7;i++){
 let number=prompt("adivine un numero entre el uno y el diez")
 if(number>=1 &&number<=10) {
-let aleatorio =Math.floor(Math.random()*10+1)
+
 
 if(number==aleatorio){
 
-  console.log("felicitaciones ese era")
+  console.log("felicitaciones ese era  "+aleatorio)
+
+break;
+
 }
+
 
 else {
+  let n=6;
 
-  console.log("Lo siento,intenta nuevamente")
+  console.log("Lo siento,intenta nuevamente.tienes "+ (n-i)+" Oportunidades" +" el numero es   "+ aleatorio)
+
 }
+
 
 }
 else{
 
   console.log("error,debe ser un numero entre uno y 10")
+}
 }
