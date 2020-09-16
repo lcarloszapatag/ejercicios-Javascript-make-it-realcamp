1212821ec57da475c29fe427afccc79f54ad4f08
lcarloszapatag <calitosf@yahoo.com>
2020-09-16 13:40:49 -0500

    ejercicio mayor igual a 10

---
+++ ejercicios-dia-2/mayor-a-10.js
@@ -0,0 +1,18 @@
+let number=prompt("escribir un numero ")
+
+
+if(number<10){
+
+console.log(" el número  "+ number + " es menor a 10")
+
+}
+if(number>10){
+
+console.log(" el número  "+ number + " es mayor a 10")
+
+}
+
+else{
+
+  console.log(" el número "+ number + " es igual 10")
+}
