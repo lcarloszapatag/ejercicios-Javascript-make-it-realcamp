7162579d8fc3bf6b12ca90ae4abeff25c2559618
lcarloszapatag <calitosf@yahoo.com>
2020-09-16 08:41:37 -0500

    arreglar number <=10

--- ejercicios-dia-2/randomNumber.js
+++ ejercicios-dia-2/randomNumber.js
@@ -1,5 +1,5 @@
 let number=prompt("adivine un numero entre el uno y el diez")
-if(number>=1 &&number<10) {
+if(number>=1 &&number<=10) {
 let aleatorio =(Math.random()*10+1);
 
 if(number==aleatorio){
