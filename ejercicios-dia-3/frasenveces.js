a7ca5454e5bd86e91480e7f69f1af72027e8384c
lcarloszapatag <calitosf@yahoo.com>
2020-09-16 21:29:48 -0500

    frase las veces que quiera el usuario

---
+++ ejercicios-dia-3/frasenveces.js
@@ -0,0 +1,11 @@
+let frase = prompt("Escribe tu frase")
+  let numero = parseInt(prompt("Escriba las veces que quiere que se repita la frase "))
+i=1;
+
+  while(i<=numero){
+
+console.log(frase,i);
+
+i++;
+
+  }
