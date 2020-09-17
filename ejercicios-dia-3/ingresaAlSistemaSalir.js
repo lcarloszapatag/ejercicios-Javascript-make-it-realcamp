4b2b7aeb0c1876e9b2f6b76c580edccdacbda31b
lcarloszapatag <calitosf@yahoo.com>
2020-09-17 10:16:42 -0500

    ejercicio 16 dia 3 curso gratuito de javascript

---
+++ ejercicios-dia-3/ingresaAlSistemaSalir.js
@@ -0,0 +1,8 @@
+let msj= prompt("1.Ingrese un mensaje al sistema")
+
+while(msj!="salir"){
+  console.log(msj)
+  msj= prompt("1.Ingrese otro  mensaje  o escriba  salir")
+ 
+}
+console.log("Hasta pronto!")
