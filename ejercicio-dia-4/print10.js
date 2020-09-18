816c768c86dc26623f91707fdb664c8bc3ec2bc8
lcarloszapatag <calitosf@yahoo.com>
2020-09-18 10:10:43 -0500

    imprimir mayores de 10 en un array

---
+++ ejercicio-dia-4/print10.js
@@ -0,0 +1,10 @@
+//const nums = [1, 23, 5, 8, 40, 12, 2, 67, 24, 9, 39]
+const nums = [10, 230, 50, 80, 40, 12, 2, 67, 24, 90, 39]
+//recorrer el array
+for (var i = 0; i < nums.length; i++) {
+
+  //imprimir los mayores de 10
+  if(nums[i]>=10){
+    console.log(nums[i]);
+  }
+}
