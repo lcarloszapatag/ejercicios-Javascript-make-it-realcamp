36c9ecfae3d765b108c040f631394a35c1f08b9c
lcarloszapatag <calitosf@yahoo.com>
2020-09-25 11:31:44 -0500

    Multiplicador con funcion map

---
+++ ejercicios-dia-9/multiplicadorMap.js
@@ -0,0 +1,5 @@
+function multiplicar( arrayOld,number){
+let newArray = arrayOld.map(arrayOld => arrayOld*number );
+//console.log(greaterTen2); // [23, 12, 45, 78, 11, 10.1, 84]
+return console.log(newArray)
+}
