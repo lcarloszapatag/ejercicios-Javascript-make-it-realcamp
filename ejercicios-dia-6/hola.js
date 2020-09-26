23bdd3e3d0464fcec8a09c014508b0ca3d441018
lcarloszapatag <calitosf@yahoo.com>
2020-09-26 08:01:44 -0500

    ejercicio d hola en cadenas javascript

---
+++ ejercicios-dia-6/hola.js
@@ -0,0 +1,11 @@
+
+function hola (cadena){
+
+return `hola ${cadena} !`
+
+}
+
+// código de prueba
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("")) // "Hola !
