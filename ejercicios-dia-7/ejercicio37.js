3a5a8707825b2b6a03c21d3ac5b87adfca698b9f
lcarloszapatag <calitosf@yahoo.com>
2020-10-06 12:38:42 -0500

    ejercicio 37 Buscar producto en Javascript

---
+++ ejercicios-dia-7/ejercicio37.js
@@ -0,0 +1,22 @@
+
+let buscarProducto=function(producto,id){
+
+
+  for(let i=0;i<producto.length; i++){
+
+    if(producto[i].id===id){
+      
+return producto[i]
+     
+    }
+else{
+return null
+}
+
+  }
+
+
+
+
+
+}
